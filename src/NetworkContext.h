f1f20342019374f99f4809d91a5b9cd8b656d452
Richardas Kuchinskas <r.kuchinskas@vk.team>
2024-08-31 14:49:54 +0300

    Network fix

--- src/NetworkContext.h
+++ src/NetworkContext.h
@@ -14,7 +14,7 @@ protected:
     boost::asio::io_context &io_context_;
     tcp::acceptor acceptor_;
     tcp::socket socket_;
-    std::vector<tcp::endpoint> peers_;
+    std::vector<std::shared_ptr<tcp::socket>> peers_;
     tcp::endpoint leaderId_;
     std::unique_ptr<Node> node_;
     boost::asio::steady_timer timer_;
@@ -35,8 +35,7 @@ protected:
                     if (o_context.notifyAll) {
                         SendMessageToAllPeers(o_context.message.value());
                     } else {
-                        SendMessageToPeer(r_context.message.sender.value(), o_context.message.value());
-                    }
+                        SendMessageToPeer(socket_by_peer(r_context.message.sender.value()), o_context.message.value());                    }
                 }
             } else if (error != boost::asio::error::operation_aborted) {
                 std::cerr << "Timer error: " << error.message() << "\n";
@@ -69,11 +68,14 @@ public:
     }
 
     void StartAccept() {
+        // socket_ set for each new accept
         acceptor_.async_accept(socket_, [this](boost::system::error_code ec) {
             if (!ec) {
                 auto peer = socket_.remote_endpoint();
-                peers_.push_back(peer);
-                std::make_shared<Session>(std::move(socket_), this)->Start();
+                auto socket = std::make_shared<tcp::socket>(std::move(socket_));
+
+                peers_.emplace_back(socket);
+                std::make_shared<Session>(socket, this)->Start();
                 std::cout << "Accepted connection from " << peer << "\n";
                 //potential node-> onAccept; <- should we understand which node?
 
@@ -83,35 +85,32 @@ public:
     }
 
     void ConnectToPeer(const std::string &host, short port) {
+        auto peer = std::make_shared<tcp::socket>(io_context_);
         tcp::resolver resolver(io_context_);
         auto endpoints = resolver.resolve(host, std::to_string(port));
-        boost::asio::async_connect(socket_, endpoints,
-                                   [this, &host, port](boost::system::error_code ec, const tcp::endpoint &) {
+        boost::asio::async_connect(*peer, endpoints,
+                                   [this, host, port, peer](boost::system::error_code ec, const tcp::endpoint &) {
                                        if (!ec) {
                                            std::cout << "Connected to peer at " << host << ":" << port << "\n";
-                                           peers_.push_back(socket_.remote_endpoint());
+                                           peers_.push_back(peer);
                                        }
                                    });
     }
 
-    void SendMessageToPeer(const tcp::endpoint &peer_endpoint, const std::string &message) {
-        auto peer_socket = std::make_shared<tcp::socket>(io_context_);
-
-        peer_socket->async_connect(peer_endpoint, [this, peer_socket, message](boost::system::error_code ec) {
-            if (!ec) {
-                boost::asio::async_write(*peer_socket, boost::asio::buffer(message),
-                                         [&message, peer_socket](boost::system::error_code ec, std::size_t length) {
-                                             if (!ec) {
-                                                 std::cout << "Message sent to peer " << peer_socket->remote_endpoint() << ": " << message << "\n";
-                                             }
-                                         });
-            }
-        });
+    void SendMessageToPeer(std::shared_ptr<tcp::socket> socket, std::string message) {
+        std::cout << "Trying send message to peer " << socket->remote_endpoint() << ": " << message
+                  << "\n";
+        boost::asio::async_write(*socket, boost::asio::buffer(message),
+                                 [message, socket](boost::system::error_code ec, std::size_t length) {
+                                     if (!ec) {
+                                         std::cout << "Message sent to peer " << socket->remote_endpoint() << ": " << message
+                                                   << "\n";
+                                     }
+                                 });
     }
 
     void SendMessageToAllPeers(const std::string &message) {
         for (const auto &peer: peers_) {
-            std::cout << "peer: " <<  peer.address() << ": " << peer.port() << "\n";
             SendMessageToPeer(peer, message);
         }
     }
@@ -136,34 +135,36 @@ public:
         if (o_context.next_time_out) {
             SetupTimer(r_context, o_context.next_time_out.value());
         }
-        if (!o_context.message.has_value()){
-            throw "2";
-        }
+
         if (o_context.notifyAll) {
             SendMessageToAllPeers(o_context.message.value());
         } else {
-            if (!r_context.message.sender.has_value()){
-                throw "1";
-            }
-
-            SendMessageToPeer(r_context.message.sender.value(), o_context.message.value());
+            SendMessageToPeer(socket_by_peer(r_context.message.sender.value()), o_context.message.value());
         }
     }
 
 private:
+    std::shared_ptr<tcp::socket> socket_by_peer(const tcp::endpoint &endpoint) {
+        for (auto &socket : peers_) {
+            if (socket->remote_endpoint() != endpoint) continue;
+            return socket;
+        }
+        // handle properly
+        throw "Some problems!";
+    }
     class Session : public std::enable_shared_from_this<Session> {
     private:
-        tcp::socket socket_;
+        std::shared_ptr<tcp::socket> socket_;
         std::string data_;
         NetworkContext *node_;
 
         void ReadMessage() {
             auto self(shared_from_this());
-            boost::asio::async_read_until(socket_, boost::asio::dynamic_buffer(data_), '\n',
+            boost::asio::async_read_until(*socket_, boost::asio::dynamic_buffer(data_), '\n',
                                           [this, self, &socket_=socket_](boost::system::error_code ec, std::size_t length) {
                                               if (!ec) {
                                                   std::cout << "Received message: " << data_ << "\n";
-                                                  node_->ReceiveMessage(data_, socket_);
+                                                  node_->ReceiveMessage(data_, *socket_);
                                                   data_.clear();
                                                   ReadMessage();
                                               }
@@ -171,8 +172,8 @@ private:
         }
 
     public:
-        Session(tcp::socket socket, NetworkContext *node)
-                : socket_(std::move(socket)), node_(node) {}
+        Session(std::shared_ptr<tcp::socket> socket, NetworkContext *node)
+                : socket_(socket), node_(node) {}
 
         void Start() {
             ReadMessage();
